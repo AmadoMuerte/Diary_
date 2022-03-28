11c183c5fbfd4914c942543ff4048678af4db36a
Amado <kisik854@gmail.com>
2022-03-28 22:01:25 +1000

    added function save in localStorage

--- js/main.js
+++ js/main.js
@@ -4,20 +4,38 @@ const btnCreate = document.querySelector('.createPage');
 const inputTitle = document.querySelector('.form__title');
 const inputDate = document.querySelector('.form__date');
 const description = document.querySelector('.description');
-
+const smileIcon = document.querySelector('.search__smile-item');
+const slimeList = document.querySelector('.search__smile-modal');
 
 let pageArray = [];
+
+
+if (localStorage.page ) {
+    pageArray = JSON.parse( localStorage.getItem('page'));
+
+    pageArray.forEach((item, index) => {
+        pageArray[index] = new Pages(item.title, item.date, item.description);
+        pageArray[index].createPage();
+    });
+} else {pageArray = [];}
+
 
 btnAdd.addEventListener('click', () => {
     showWindow();
 });
 
+
+
+
+
 btnCreate.addEventListener('click', () => {
     showWindow();
     let page = new Pages( inputTitle.value, inputDate.value, description.value );
+    pageArray.push(page);
     page.createPage();
-    console.log(page);
-
+    clearForm();
+    updateLocal();
+    console.log(pageArray);
 });
 
 
@@ -26,6 +44,13 @@ function showWindow() {
     document.querySelector('.main').classList.toggle('invisibleMain');
     document.querySelector('.search').classList.toggle('invisibleMain');
 }
+
+function clearForm() {
+    inputTitle.value = '';
+    inputDate.value = '';
+    description.value = '';
+    smileIcon.innerHTML = '';
+};
 
 function updateLocal() {
     localStorage.setItem('page', JSON.stringify(pageArray)); 
